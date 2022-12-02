5e6277a49f800337327b2bab11a72c4c2a77b534
David Gage <whytheplatypus@gmail.com>
2022-12-02 10:07:34 -0500

    day 1 problem 2

--- day-1-rust/src/main.rs
+++ day-1-rust/src/main.rs
@@ -8,17 +8,27 @@ fn main() {
     let input = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
     //println!("{}", input);
-    println!("{}", biggest_horder(&input));
+    let horders = biggest_horders(&input);
+    println!("biggest horder: {}", horders[0]);
+    let top_3 = dbg!(horders[0..3].to_vec());
+    println!("biggest three total: {}", top_3.iter().sum::<i32>());
 }
 
-fn biggest_horder(input: &str) -> i32 {
+fn biggest_horders(input: &str) -> Vec::<i32> {
     let raw_elves = split_elves(input);
     let mut elves = Vec::<Vec::<i32>>::new();
     for raw_elf in raw_elves {
         elves.push(read_elf(raw_elf).unwrap());
     }
-    let max_calories = elves.iter().map(|elf| elf_total_cals(elf)).max();
-    max_calories.unwrap()
+    let totals = elves.iter().map(|elf| elf_total_cals(elf));
+    let mut sorted_totals = totals.collect::<Vec::<i32>>();
+    sorted_totals.sort();
+    sorted_totals.reverse();
+    sorted_totals
+}
+
+fn biggest_horder(elves: Vec::<i32>) -> i32 {
+    elves[0]
 }
 
 fn split_elves(input: &str) -> impl Iterator::<Item = &str> {
@@ -59,7 +69,7 @@ const TEST_INPUT: &str = "1000
 
 #[test]
 fn test_biggest_horder() {
-    assert_eq!(biggest_horder(TEST_INPUT), 24000);
+    assert_eq!(biggest_horder(biggest_horders(TEST_INPUT)), 24000);
 }
 
 #[test]
